50bef409d33adaf717c57ea0079684a173d052e0
Armando Rigual <mandydark3@gmail.com>
2023-03-16 20:50:01 -0600

    Creating: DroneController file. Implementing: Check drone battery level for a given drone.

---
+++ Controllers/DroneController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusalaDrones.Data;
+
+namespace MusalaDrones.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DroneController : ControllerBase
+    {
+        private readonly ILogger<DroneController> _logger;
+        private MusalaDronesDbContext _context;
+
+        public DroneController(ILogger<DroneController> logger, MusalaDronesDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+
+        // Check drone battery level for a given drone
+        [HttpGet(Name = "GetDroneBatteryLevel")]
+        public JsonResult GetDroneBatteryLevel(string droneSN)
+        {
+            var drone = _context.Drones.FirstOrDefault(p => p.SerialNumber == droneSN);
+            if (drone == null)
+                return new JsonResult(JsonResults.JSON_NOTFOUND);
+
+            return new JsonResult(drone.BatteryCapacity);
+        }
+    }
+}
